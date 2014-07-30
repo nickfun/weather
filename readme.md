9a21b49eb5444ed1f374a7ce6d84ee0f1a11fb9e
Nick F <nick@nick.gs>
2014-07-29 19:51:51 -0700

    init commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Weather
+
+## About
+
+Simple python app to scrape weather data and do some processing on it
+
+## Goals
+
+* Learn Python
+* Learn data processing
+* Learn batch processing of data
---
+++ slurp.py
@@ -0,0 +1,17 @@
+# download the public json file
+#
+# Nick F 
+# Tue Jul 29 19:25:21 PDT 2014
+
+import urllib
+import datetime
+
+timeNow = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+localFile = timeNow + ".json"
+urllib.urlretrieve("http://www.weatherlink.com/mapstations.json", localFile);
+
+with open("log.txt", "a") as myfile:
+    myfile.write(timeNow + " - Downloaded json file\n")
+
+print "Done!"
+
