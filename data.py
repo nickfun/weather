c56d2e685b8917985e44ab6e24bdf7fa528fd4d0
Nick Funnell <nick@nick.gs>
2014-08-08 20:59:53 +0000

    can parse the filename for timestamp

--- data.py
+++ data.py
@@ -1,8 +1,19 @@
+from datetime import datetime
 import math
 import sys
 import json
 
 files = []
+
+def getDate(sDate):
+    oTime = datetime.strptime(sDate, "%Y-%m-%d_%H-%M.json")
+    sNewDate = oTime.strftime("%c")
+    return sNewDate
+
+def toUnix(sDate):
+    oTime = datetime.strptime(sDate, "%Y-%m-%d_%H-%M.json")
+    dt0 = datetime(1970,1,1)
+    return (oTime - dt0).total_seconds()
 
 for arg in sys.argv:
     if arg[-5:] == ".json":
@@ -11,7 +22,10 @@ for arg in sys.argv:
 firstfile = files.pop(0)
 with open(firstfile, "r") as jsonin:
     data = json.loads(jsonin.read())
-    print data[0]
     print data[3]
+
+for sDate in files:
+    mydate = getDate(sDate)
+    print "%s becomes %s unix: %d" % (sDate, mydate, toUnix(sDate))
 
 print "Done"
