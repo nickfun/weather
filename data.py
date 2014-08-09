e1331bd990e7c06adc92f04196245fa266968a1a
Nick Funnell <nick@nick.gs>
2014-08-09 00:30:20 +0000

    code to insert into Riak, but having openssl errors. might need to reinstall riak

--- data.py
+++ data.py
@@ -1,31 +1,38 @@
 from datetime import datetime
+import riak
 import math
 import sys
 import json
 
 files = []
+rc = riak.RiakClient(pb_port=8087, protocol='pbc')
 
-def getDate(sDate):
-    oTime = datetime.strptime(sDate, "%Y-%m-%d_%H-%M.json")
-    sNewDate = oTime.strftime("%c")
-    return sNewDate
+def filenameToDate(sDate):
+    return datetime.strptime(sDate, "%Y-%m-%d_%H-%M.json")
 
-def toUnix(sDate):
-    oTime = datetime.strptime(sDate, "%Y-%m-%d_%H-%M.json")
+
+def filenameToUnix(sDate):
+    oTime = filenameToDate(sDate)
     dt0 = datetime(1970,1,1)
     return (oTime - dt0).total_seconds()
 
+# Of the filenames passed to us, keep the ones that end in .json
 for arg in sys.argv:
     if arg[-5:] == ".json":
         files.append(arg)
 
-firstfile = files.pop(0)
-with open(firstfile, "r") as jsonin:
-    data = json.loads(jsonin.read())
-    print data[3]
+# For all those files, read the files and insert the data into the database
+for sFile in files:
+    oDate = filenameToDate(sFile)
+    iDate = filenameToUnix(sFile)
+    with open(sFile, "r") as inputFile:
+        print "Begin: %s" % sFile
+        data = json.load(inputFile)
+        for station in data:
+            bucket = rc.bucket(station['DID'])
+            key = bucket.new(iDate, data=station)
+            key.store()
+            print "store: %s" % station['uname']
+        print "Done: %s" % sFile
 
-for sDate in files:
-    mydate = getDate(sDate)
-    print "%s becomes %s unix: %d" % (sDate, mydate, toUnix(sDate))
-
-print "Done"
+print "Done: ALL"
