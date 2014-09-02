fb5551a08ca54272e5549d1950daf0f933f4088b
Nick Funnell <nick@nick.gs>
2014-09-02 06:03:46 +0000

    progress output

--- slurp.py
+++ slurp.py
@@ -6,11 +6,13 @@
 import urllib
 import datetime
 
+logFile = "/home/nick/weather/log.txt"
 timeNow = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 localFile = "/home/nick/weather/" + timeNow + ".json"
+print "Begin file load"
 urllib.urlretrieve("http://www.weatherlink.com/mapstations.json", localFile);
-
-with open("/home/nick/weather/log.txt", "a") as myfile:
+print "Write to log file"
+with open(logFile, "a") as myfile:
     myfile.write(timeNow + " - Downloaded json file\n")
 
 print "Done!"
